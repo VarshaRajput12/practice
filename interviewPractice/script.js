5273807497c3637fe44b43aca95988934e5539e1
VarshaRajput12 <varsharajput353@gmail.com>
2023-01-15 17:24:21 +0530

    practice

--- interviewPractice/script.js
+++ interviewPractice/script.js
@@ -105,22 +105,104 @@
 
 // this keword 
 
-let obj = {
-    Name : "varsha",
-    surName : "rajput",
-    age :21,
+// let obj = {
+//     Name : "varsha",
+//     surName : "rajput",
+   
+// }
+
+// function fun(age){
+//     return `${this.Name} ${this.surName}  my age is ${age}`;
+// }
+// let obj2 = {
+//     Name : "kajal",
+//     surName : "rajput",
+  
+// }
+
+// console.log(fun.call(obj, 21))
+// console.log(fun.call(obj2))
+
+// let bindFun = fun.bind(obj, 21) 
+// console.log(bindFun())
+
+//===============================================================
+// promise
+// function promise(arr){
+//     return new Promise((resolve,reject)=>{
+//         if(arr.length > 5){
+//             reject("rejected")
+//         }else{
+//             resolve("resolved")
+//         }
+//     })
+// }
+// promise([1,2,3,4,5,6,7]).then((result)=>{
+//     console.log(result)
+// }).catch((err)=>{
+//     console.log(err)
+// })
+
+//=======================================================================
+
+
+// local storage
+
+// let arr = [1,2,3,4,5,6,7,8];
+
+// let store = localStorage.setItem("key", JSON.stringify(arr));
+
+// let get = JSON.parse(localStorage.getItem("key"))
+
+// console.log(get)
+
+//==================================================================
+
+//closure
+
+// function abc(){
+//     const a = 100;
+//     function xyz(){
+//         const b = 2
+//         function inner(){
+//             console.log(a *b)
+//         }
+//         inner()
+//     }
+//     xyz()
+// }
+// abc()
+
+// ==================================================================
+
+// currying
+
+// function a(x){
+//     return function(y){
+//         return function(z){
+//             return(x*y*z);
+//         }
+//     }
+// }
+// console.log(a(2)(3)(4))
+
+// event bubbling and capturing
+
+const maincontainer = document.querySelector(".maincontainer");
+const container = document.querySelector(".container");
+const innercontainer = document.querySelector(".innercontainer");
+
+function outerMost(){
+  alert("grandparent")
+}
+function outer(){
+  alert("parent");
+}
+function inner(e){
+    alert("child")
+    e.stopPropagation()
 }
 
-function fun(){
-    return(`${this.Name} ${this.surName}`)
-}
-let obj2 = {
-    Name : "kajal",
-    surName : "rajput",
-    age :21,
-}
-
-
-
-console.log(fun.call(obj))
-console.log(fun.call(obj2))
+maincontainer.addEventListener("click", outerMost);
+container.addEventListener("click", outer)
+innercontainer.addEventListener("click", inner)
